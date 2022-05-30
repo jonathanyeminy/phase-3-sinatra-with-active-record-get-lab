a5d115150e321f2b88865706382e6c9a9217b4e0
Jonathan Yeminy <jonathanyeminy@yahoo.com>
2022-05-30 11:54:05 -0400

    commit tree 5c0171497a51b11f585092ffab375869a2150517

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,29 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
-  # add routes
+    get '/bakeries' do
+      bakeries = Bakery.all
+      bakeries.to_json
+    end
+
+    get '/bakeries/:id' do
+      bakery = Bakery.find(params[:id])
+      bakery.to_json(include: :baked_goods)
+    end
+
+    get '/baked_goods' do
+      baked_goods = BakedGood.all
+      baked_goods.to_json
+    end
+
+    get '/baked_goods/by_price' do
+      baked_goods_by_price = BakedGood.all.sort_by{|h| h[:price]*-1}
+      baked_goods_by_price.to_json
+    end
+
+    get '/baked_goods/most_expensive' do
+      baked_goods_by_price = BakedGood.all.max_by(&:price)
+      baked_goods_by_price.to_json
+    end
 
 end
